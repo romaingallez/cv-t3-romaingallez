f8297764bd8fede2037e10f628c5738a883a5d36
romaingallez <74435156+romaingallez@users.noreply.github.com>
2023-06-24 12:34:43 +0200

    public

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { init } from "@socialgouv/matomo-next";
+import React, { useEffect } from "react";
+
+
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL || "https://matomo.romaingallez.fr";
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID || "6";
+
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  useEffect(() => {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }, []);
+
   return <Component {...pageProps} />;
 };
 
 export default api.withTRPC(MyApp);
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,15 @@
 import { type Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+			fontFamily: {
+				sans: ['Questrial',  ...defaultTheme.fontFamily.sans],
+			  },		
+		},
   },
   plugins: [],
 } satisfies Config;
