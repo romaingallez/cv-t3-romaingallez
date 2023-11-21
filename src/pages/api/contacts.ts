0d14376a95ab62f3e1f88857264e379d99f72568
Your Name <you@example.com>
2023-11-21 11:03:44 +0100

    Contact page rework with new theme and API ========================================== Changing the theme of the contact page to match the new theme of the site. Also adding a new API endpoint to handle the contact form to also send a notification using pushover.

---
+++ src/pages/api/contacts.ts
@@ -0,0 +1,84 @@
+// src/pages/api/contacts.ts
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { ReactEmailTemplate } from "src/components/ReactEmailTemplate";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+// Function to send a notification via Pushover
+const sendPushoverNotification = async (message: string) => {
+  const form = new FormData();
+  form.append("token", process.env.PUSHOVER_TOKEN as string);
+  form.append("user", process.env.PUSHOVER_USER as string);
+  form.append("message", message);
+
+  const options = {
+    method: 'POST',
+    body: form,
+  };
+
+  try {
+    const response = await fetch('https://api.pushover.net/1/messages.json', options);
+    const data = await response.json();
+    console.log('Pushover response:', data);
+  } catch (err) {
+    console.error('Error sending Pushover notification:', err);
+  }
+};
+
+const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    res.status(405).json({ error: 'Method Not Allowed' });
+    return;
+  }
+
+  const { to, from, name, subject, body, captchaToken } = req.body;
+
+  // Validate the required parameters
+  if (!to || !from || !subject || !body || !captchaToken) {
+    res.status(400).json({ error: 'Missing parameters' });
+    return;
+  }
+
+  // Captcha verification
+  const form = new URLSearchParams();
+  form.append('secret', process.env.TURNSTILE_SECRET_KEY ?? "");
+  form.append('response', captchaToken);
+  form.append('remoteip', req.headers['x-forwarded-for'] as string);
+
+  const captchaResult = await fetch(
+    'https://challenges.cloudflare.com/turnstile/v0/siteverify',
+    { method: 'POST', body: form }
+  );
+
+  if (!captchaResult.ok) {
+    res.status(captchaResult.status).json({ error: 'Failed to verify captcha token' });
+    return;
+  }
+
+  const captchaJson = await captchaResult.json() as { success: boolean };
+  
+  if (!captchaJson.success) {
+    res.status(400).json({ error: 'Captcha token failed' });
+    return;
+  }
+
+  // Sending email
+  try {
+    const emailResponse = await resend.emails.send({
+      from: "Romain GALLEZ <contact@romaingallez.fr>",
+      to: ["romain.gallez@gmail.com"],
+      subject: subject,
+      react: ReactEmailTemplate({ name: name, from: from, subject: subject, body: body }),
+    });
+
+    // Sending notification to Pushover
+    await sendPushoverNotification(`New message from ${name}: ${body}`);
+    
+    res.status(200).json(emailResponse);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to send email' });
+  }
+};
+
+export default sendEmail;
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,191 +1,158 @@
 import Head from 'next/head';
-import { useState, useRef, ChangeEvent, FormEvent } from 'react';
+import Image from 'next/image';
+import "@fontsource/questrial";
+import { useState, useRef } from 'react';
 import axios from 'axios';
 import type { TurnstileInstance } from '@marsidev/react-turnstile';
-import CloudFlareCaptcha  from 'src/components/CloudFlareCaptcha';
+import CloudFlareCaptcha from 'src/components/CloudFlareCaptcha';
 import MetaTags from 'src/components/MetaTags';
 import CustomFooter from 'src/components/CustomFooter';
 
 export default function Contact() {
   // Form field states
-  const [name, setName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [message, setMessage] = useState<string>('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
   
-  // Form status states
-  const [isSending, setIsSending] = useState<boolean>(false);
-  const [isSent, setIsSent] = useState<boolean>(false);
-  const [error, setError] = useState<string>('');
-
-  // Captcha states
+  // Form status and Captcha states
+  const [isSending, setIsSending] = useState(false);
+  const [isSent, setIsSent] = useState(false);
+  const [error, setError] = useState('');
   const captcha = useRef<TurnstileInstance | null>(null);
-  const [captchaSuccess, setCaptchaSuccess] = useState<boolean>(false);
+  const [captchaSuccess, setCaptchaSuccess] = useState(false);
   const [token, setToken] = useState<string | null>(null);
 
-  // // Field change handlers
-  // const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => setName(event.target.value);
-  // const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value);
-  // const handleMessageChange = (event: ChangeEvent<HTMLTextAreaElement>) => setMessage(event.target.value);
-
   // Form submission handler
-  const handleSubmit = (event: FormEvent) => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-  
-    // Check captcha
     if (!captcha || !captchaSuccess) {
       setError('Please complete the captcha challenge.');
       return;
     }
-  
-    // Begin sending
-    beginSending();
-  
-    // Send the email
-    sendEmail().then(() => {
-      // Clear form on success
-      clearForm();
-    }).catch(() => {
-      // Show error on failure
-      setError('Failed to send email. Please try again later.');
-    }).finally(() => {
-      // End sending
-      endSending();
-    });
-  };
-  
-
-  const beginSending = () => {
     setIsSending(true);
     setError('');
-  };
 
-  const endSending = () => {
-    setIsSending(false);
-  };
-
-  const sendEmail = async () => {
-    await axios.post('/api/email', {
-      to: 'romain@romaingallez.fr',
-      from: email,
-      subject: 'Contact Form Submission',
-      captchaToken: token,
-      body: message,
-      name: name,
-    });
-  };
-
-  const clearForm = () => {
-    setName('');
-    setEmail('');
-    setMessage('');
-    setIsSent(true);
+    try {
+      await axios.post('/api/contacts', {
+        to: 'romain@romaingallez.fr',
+        from: email,
+        subject: 'Contact Form Submission',
+        captchaToken: token,
+        body: message,
+        name: name,
+      });
+      setName('');
+      setEmail('');
+      setMessage('');
+      setIsSent(true);
+    } catch {
+      setError('Failed to send email. Please try again later.');
+    } finally {
+      setIsSending(false);
+    }
   };
 
   return (
     <>
-      <MetaTags 
-        title="Contacter Romain" 
-        description="Vous pouvez me contacter par mail ou via le formulaire de contact." 
-        image="https://cv.romaingallez.fr/resume.png" 
-        url="https://cv.romaingallez.fr" 
+      <MetaTags
+        title="Contacter Romain"
+        description="Vous pouvez me contacter par mail ou via le formulaire de contact."
+        image="https://cv.romaingallez.fr/resume.png"
+        url="https://cv.romaingallez.fr"
       />
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main
-        id="wrapper"
-        className="flex-row flex-wrap shadow-2xl sm:m-12 sm:flex-nowrap"
-      >
-        <div className="flex w-full flex-col p-4">
-          <p>
-          Salut !
-          <br />
-          Je suis ravi de savoir que vous souhaitez me contacter. N'hésitez pas à remplir le formulaire ci-dessous pour me transmettre vos coordonnées et votre message.
-          <br />
-          Je suis impatient de vous lire !
-          <br />
-          Romain
-          </p>
-          <form
-            className="mx-auto w-full max-w-md p-5"
-            onSubmit={handleSubmit}
-          >
-            <div className="mb-4">
-              <label
-                htmlFor="name"
-                className="mb-2 block text-lg text-gray-700"
-              >
-                Nom
-              </label>
-              <input
-                type="text"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="w-full rounded-md border border-gray-300 px-4 py-2 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
-                required
-              />
+  
+      <div className="font-questrial container mx-auto max-w-screen-xl w-full px-4 sm:px-0">
+        <main
+          id="wrapper"
+          className="flex-row flex-wrap shadow-2xl sm:m-12 flex-col sm:flex-row"
+        >
+          <div className="w-full bg-white p-8">
+            <div className="prose">
+              <h2 className="border-l-8 border-green-500 pl-2 font-bold uppercase tracking-widest sm:text-xl mb-2">
+                Contactez-moi
+              </h2>
+              <p>
+                Salut ! Je suis ravi de savoir que vous souhaitez me contacter.
+                N'hésitez pas à remplir le formulaire ci-dessous.
+              </p>
+              <form onSubmit={handleSubmit} className="mt-6">
+                {/* Name Field */}
+                <div className="mb-4">
+                  <label htmlFor="name" className="block text-lg font-medium">
+                    Nom
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="mt-1 block w-full rounded-md border-2 border-gray-300 focus:border-indigo-500 shadow-md focus:shadow-lg transition duration-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                    required
+                  />
+                </div>
+  
+                {/* Email Field */}
+                <div className="mb-4">
+                  <label htmlFor="email" className="block text-lg font-medium">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="mt-1 block w-full rounded-md border-2 border-gray-300 focus:border-indigo-500 shadow-md focus:shadow-lg transition duration-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                    required
+                  />
+                </div>
+  
+                {/* Message Field */}
+                <div className="mb-4">
+                  <label htmlFor="message" className="block text-lg font-medium">
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    className="mt-1 block w-full rounded-md border-2 border-gray-300 focus:border-indigo-500 shadow-md focus:shadow-lg transition duration-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                    rows={4}
+                    required
+                  />
+                </div>
+  
+                {/* Captcha */}
+                <div className="mb-6">
+                  <CloudFlareCaptcha
+                    ref={captcha}
+                    onSuccess={(token: string) => {
+                      setToken(token);
+                      setCaptchaSuccess(true);
+                    }}
+                  />
+                </div>
+  
+                {/* Submit Button */}
+                <button
+                  type="submit"
+                  disabled={isSending}
+                  className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  {isSending ? 'Envoi en cours...' : 'Envoyer'}
+                </button>
+  
+                {/* Status Messages */}
+                {isSent && <p className="mt-2 text-green-600">Email envoyé avec succès !</p>}
+                {error && <p className="mt-2 text-red-600">{error}</p>}
+              </form>
             </div>
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="mb-2 block text-lg text-gray-700"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full rounded-md border border-gray-300 px-4 py-2 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="message"
-                className="mb-2 block text-lg text-gray-700"
-              >
-                Message
-              </label>
-              <textarea
-                id="message"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                className="w-full resize-none rounded-md border border-gray-300 px-4 py-2 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
-                rows={4}
-                required
-              ></textarea>
-            </div>
-
-            <div className="mb-4">
-              <CloudFlareCaptcha
-                ref={captcha}
-                onSuccess={(token: string) => {
-                  setToken(token);     
-                  setCaptchaSuccess(true)
-                }}              
-              />
-            </div>
-            <div className="text-center">
-              <button
-                type="submit"
-                className="rounded-md bg-indigo-500 px-6 py-3 text-lg text-white hover:bg-indigo-600"
-                disabled={isSending}
-              >
-                {isSending ? "Sending..." : "Envoyer"}
-              </button>
-            </div>
-            {isSent && (
-              <p className="text-green-500 mt-2">Email sent successfully!</p>
-            )}
-            {error && <p className="text-red-500 mt-2">{error}</p>}
-          </form>
-        </div>
-      </main>
-
-    <CustomFooter />
+          </div>
+        </main>
+        <CustomFooter />
+      </div>
     </>
   );
 }
