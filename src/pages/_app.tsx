57f86a1a5952b071d24d5a5f63c863adf03e9037
romaingallez <74435156+romaingallez@users.noreply.github.com>
2024-01-30 16:54:19 +0100

    Add @vercel/analytics and @vercel/speed-insights dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,10 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { init } from "@socialgouv/matomo-next";
 import React, { useEffect } from "react";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+
 
 
 const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL || "https://matomo.romaingallez.fr";
@@ -14,8 +18,15 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
   }, []);
 
-  return <Component {...pageProps} />;
-};
+  // return <Component {...pageProps} />;
+  return (
+    <>
+    <Component {...pageProps} />
+    <Analytics />
+    <SpeedInsights />
+    </>
+  );
+}
 
 export default api.withTRPC(MyApp);
 
